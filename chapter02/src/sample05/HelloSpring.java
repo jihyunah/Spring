508418c8aa9f59f80915a3315302b63cb7a8999e
이지현 <121652350+jihyunah@users.noreply.github.com>
2023-04-07 12:31:30 +0900

    04.07.12:30

--- chapter02/src/sample05/HelloSpring.java
+++ chapter02/src/sample05/HelloSpring.java
@@ -1,10 +1,14 @@
 package sample05;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class HelloSpring {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+		MessageBean messageBean = (MessageBean) context.getBean("messageBeanImpl2");
+		messageBean.helloCall();
 	}
 
 }
--- chapter02/src/sample05/MessageBeanImpl.java
+++ chapter02/src/sample05/MessageBeanImpl.java
@@ -1,13 +1,34 @@
 package sample05;
 
+
 public class MessageBeanImpl implements MessageBean {
 	private String name;
 	private String phone;
+	private Outputter outputter;
 	
+
+	public void setOutputter(Outputter outputter) {
+		System.out.println("setOutPutter(Outputter outputter)");
+		this.outputter = outputter;
+	}
+
+
+	public MessageBeanImpl(String name) {
+		System.out.println("MessageBeanImpl(String name)");
+		this.name = name;
+	}
+
+
+	public void setPhone(String phone) {
+		System.out.println("setPhone(String phone)");
+		this.phone = phone;
+	}
+
+
 	@Override
 	public void helloCall() {
-		// TODO Auto-generated method stub
-		
+		System.out.println("helloCall()");
+		outputter.output(name + "\t" + phone);
 	}
 
 }
