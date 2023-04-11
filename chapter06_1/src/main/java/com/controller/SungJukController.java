1baaeb353f2c14fe49012ba67f1651362cfe3b95
이지현 <121652350+jihyunah@users.noreply.github.com>
2023-04-11 16:39:08 +0900

    04.11.16:30

--- chapter06_1/src/main/java/com/controller/SungJukController.java
+++ chapter06_1/src/main/java/com/controller/SungJukController.java
@@ -5,18 +5,20 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.bean.SumDTO;
 import com.bean.SungJukDTO;
 
 @Controller
+@RequestMapping(value = "sungJuk") //반복되는 namespace
 public class SungJukController {
-	@GetMapping(value="/sungJuk/input.do")
+	@GetMapping(value="input.do")
 	public String input() {
 		return "sungJuk/input"; // => /WEB-INF/sum/input.jsp
 	}
 	
-	@PostMapping(value="/sungJuk/result.do")
+	@PostMapping(value="result.do")
 	public String result(@ModelAttribute SungJukDTO sungJukDTO, Model model) { //modelMap에 실려 보내겠습니다.
 		int tot = sungJukDTO.getKor()+ sungJukDTO.getEng()+sungJukDTO.getMath();
 		double avg = tot/3.;
