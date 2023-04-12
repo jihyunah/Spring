0ddbeddf424a22ea15f4b1993a3dbf08a8372a87
이지현 <121652350+jihyunah@users.noreply.github.com>
2023-04-12 09:47:07 +0900

    04.12.9:40

--- chapter06_SpringWebMaven/src/main/java/user/controller/UserController.java
+++ chapter06_SpringWebMaven/src/main/java/user/controller/UserController.java
@@ -1,13 +1,21 @@
 package user.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-public class UserController {
-	@RequestMapping(value="/user/writeForm/", method = RequestMethod.GET)
-	public String writeForm() {
-		return "user/writeForm";
+//public class UserController {
+//	@RequestMapping(value="/user/writeForm/", method = RequestMethod.GET)
+//	public String writeForm() {
+//		return "user/writeForm";
+//	}
+
+@RequestMapping(value="user")
+	public class UserController {
+		@GetMapping(value="writeForm")
+		public String writeForm() {
+			return "user/writeForm";
 	}
 }
--- chapter06_SpringWebMaven/src/main/webapp/WEB-INF/index.jsp
+++ chapter06_SpringWebMaven/src/main/webapp/WEB-INF/index.jsp
@@ -9,7 +9,7 @@
 <body>
 <h3>*** 메인화면 ***</h3>
 <h4>
-	<p><a href="./user/writeForm/">입력</a></p>
+	<p><a href="./user/writeForm">입력</a></p>
 	<p><a href="">출력</a></p>
 	<p><a href="">수정</a></p>
 	<p><a href="">삭제</a></p>
