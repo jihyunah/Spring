ee53261fba6b648f0760f4f760bcb0740738c9b6
이지현 <121652350+jihyunah@users.noreply.github.com>
2023-04-14 11:45:20 +0900

    04.14.11:40

--- chapter06_SpringWebMaven/src/main/java/user/controller/UserController2.java
+++ chapter06_SpringWebMaven/src/main/java/user/controller/UserController2.java
@@ -3,6 +3,7 @@ package user.controller;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.List;
 
 import javax.servlet.http.HttpSession;
 
@@ -10,12 +11,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
+import user.bean.UserImageDTO;
 import user.service.UserService;
 
 
@@ -31,31 +34,111 @@ public class UserController2 {
 	public String uploadForm() { //주소로 가라. 
 		return "user/uploadForm";
 	}
+
+//// -----------------name="img" 1개일 때 ----------------------
+//	@PostMapping(value="upload", produces = "text/html; charset=UTF-8")
+//	@ResponseBody
+//	public String upload(@RequestParam MultipartFile img, //서버로 가 
+//						 @ModelAttribute UserImageDTO userImageDTO, //dto로 가고 
+//						 HttpSession session) { //spring에게 session 객체좀 주세요.하는거다. 
+//		//가상 폴더 
+//		//String filePath_lier = "/Applications/study/Spring/workspace/Spring/chapter06_SpringWebMaven/src/main/webapp/WEB-INF/storage";
+//		
+//		String filePath = session.getServletContext().getRealPath("/WEB-INF/storage");
+//		System.out.println("실제 폴더 = " + filePath);
+////		/Applications/study/Spring/workspace/Spring/.metadata/.plugins/org.eclipse.wst.server.core/tmp0/wtpwebapps/chapter06_SpringWebMaven/WEB-INF/storage
+//		
+//		String fileName = img.getOriginalFilename();
+//		System.out.println("파일명 = " + fileName);
+//		
+//		File file = new File(filePath, fileName); //파일 생성 
+//		//File file_lier = new File(filePath_lier, fileName);
+//		
+//		try {
+//			//FileCopyUtils.copy(img.getInputStream(), new FileOutputStream(file_lier)); //파일 복사 
+//			img.transferTo(file); //파일 이동 
+//			
+//			
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//		}
+//		
+//		userImageDTO.setImage1(fileName);
+//		
+//		//UserService --> UserDAO __> userMapper.xml(저장시키기)
+//		
+//		return "<img src='../storage/" + fileName + "' width=300' height='300' />";
+//	}
 	
-	@PostMapping(value="upload")
-	@ResponseBody
-	public String upload(@RequestParam MultipartFile img, HttpSession session) { //spring에게 session 객체좀 주세요.하는거다. 
-		//가상 폴더 
-		String filePath_lier = "/Applications/study/Spring/workspace/Spring/chapter06_SpringWebMaven/src/main/webapp/WEB-INF/storage";
-		
-		String filePath = session.getServletContext().getRealPath("/WEB-INF/storage");
-		System.out.println("실제 폴더 = " + filePath);
-//		/Applications/study/Spring/workspace/Spring/.metadata/.plugins/org.eclipse.wst.server.core/tmp0/wtpwebapps/chapter06_SpringWebMaven/WEB-INF/storage
-		
-		String fileName = img.getOriginalFilename();
-		
-		File file = new File(filePath, fileName); //파일 생성 
-		File file_lier = new File(filePath_lier, fileName);
-		
-		try {
-			FileCopyUtils.copy(img.getInputStream(), new FileOutputStream(file_lier)); //파일 복사 
-			img.transferTo(file);
+	
+//	// -----------------name= "img" 2개 이상 일 때 ----------------------
+//	@PostMapping(value="upload" , produces = "text/html; charset=UTF-8")
+//    @ResponseBody
+//    public String upload(@RequestParam MultipartFile[] img,
+//                         @ModelAttribute UserImageDTO userImageDTO,
+//                         HttpSession session){
+//       
+//       //가상 폴더
+//       String filePath_lier = "/Applications/study/Spring/workspace/Spring/chapter06_SpringWebMaven/src/main/webapp/WEB-INF/storage";
+////			//진짜 폴더
+//       String filePath = session.getServletContext().getRealPath("/WEB-INF/storage");
+//       System.out.println("실제 폴더 = " + filePath);
+//       
+//       String fileName;
+//       File file;
+//       
+//       if(img[0] != null) {
+//          fileName = img[0].getOriginalFilename();
+//          file = new File(filePath, fileName);
+//          
+//          try {
+//             img[0].transferTo(file);
+//          } catch (IOException e) {
+//             e.printStackTrace();
+//          } //실제 경로에 파일을 복사해라
+//       }
+//       
+//       if(img[1] != null) {
+//          fileName = img[1].getOriginalFilename();
+//          file = new File(filePath, fileName);
+//          
+//          try {
+//             img[1].transferTo(file);
+//          } catch (IOException e) {
+//             e.printStackTrace();
+//          } //실제 경로에 파일을 복사해라
+//       }
+//       
+//       return "이미지 등록 완료";
+//    }
+	
+// ----------------- 한 번에 여러개의 파일을 선택  ----------------------
+		@PostMapping(value="upload", produces = "text/html; charset=UTF-8")
+		@ResponseBody
+		public String upload(@RequestParam("img[]") List<MultipartFile> list, //서버로 가 
+							 @ModelAttribute UserImageDTO userImageDTO, //dto로 가고 
+							 HttpSession session) { //spring에게 session 객체좀 주세요.하는거다. 
+				
+			String filePath = session.getServletContext().getRealPath("/WEB-INF/storage");
+			System.out.println("실제 폴더 = " + filePath);
+				
+			String fileName; 
+			File file; //파일 생성 
 			
-			
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		return "<img src='../storage/" + fileName + "' width=300' height='300' />";
-	}
+			for(MultipartFile img : list) {
+				fileName = img.getOriginalFilename();
+				file = new File(filePath, fileName);
+				
+				try {
+					img.transferTo(file);
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} //파일 이동 
+			} //for
+				
+			return "이미지 등록 완료";	
+					
+			}
 
 }
--- chapter06_SpringWebMaven/src/main/webapp/WEB-INF/user/uploadForm.jsp
+++ chapter06_SpringWebMaven/src/main/webapp/WEB-INF/user/uploadForm.jsp
@@ -9,26 +9,87 @@
 <body>
 <!-- 1. 단순 submit으로 넘겨 -->
 <form method="post" enctype="multipart/form-data" action="/chapter06_SpringWebMaven/user/upload">
-	<input type="file" name="img">
-	<br/><br/>
-	<input type="submit" name="uploadBtn" value="이미지 등록">
-	
+	<table border="1">
+		<tr>
+			<td>상품명</td>
+			<td><input type="text" name="imageName" size="35"></td>
+		</tr>
+		
+		<tr>
+			<td colspan="2">
+				<textarea name="imageContent" rows="15" cols="50"></textarea>
+			</td>	
+		</tr>
+		
+		<!-- 다중 파일 업로드 시에는 name 속성에 같은 이름을 지정해야 한다 -->
+		
+		<!-- <tr>
+			<td colspan="2">
+				<input type="file" name="img">
+			</td>
+		</tr>
+		
+		<tr>
+			<td colspan="2">
+				<input type="file" name="img">
+			</td>
+		</tr>  -->
+		
+		
+		<tr>
+			<td colspan="2">
+				<!-- 업로드 버튼을 누르기 전에 카메라 아이콘을 통해서 선택한 이미지가 맞는지 확인하기 위해서 이미지를 보이게 한다. -->
+				<img id="showImg" width="70" height="70">
+			
+				<img id="camera" alt="카메라" src="../image/camera.png" width="30" height="30">
+				<input type="file" name="img[]" id="img" multiple="multiple" style="visibility: hidden;"> 
+			</td>
+		</tr> 
+		
+		<tr>
+			<td colspan="2" align="center">
+				<input type="submit" id="uploadBtn" value="이미지 등록">
+			</td>
+		</tr>
+	</table>
 </form>
 
+<script type="text/javascript" src="http://code.jquery.com/jquery-3.6.4.min.js"></script>
+<script type="text/javascript">
+$('#camera').click(function(){
+	//강제 이벤트 발생 
+	$('#img').trigger('click');
+});
+
+<!-- 업로드 버튼을 누르기 전에 카메라 아이콘을 통해서 선택한 이미지가 맞는지 확인하기 위해서 이미지를 보이게 한다. -->
+$('#img').change(function(){
+	readURL(this); //이벤트 발생한 나 img를 들고 글로 이동해. 
+});
+
+function readURL(input){
+	var reader = new FileReader(); //자바 스크립트 객체임 
+	
+	reader.onload = function(e){	//읽어들여라 
+		$('#showImg').attr('src', e.target.result); //e.target => event 발생한 아이의 결과를 가져와라 
+	} 
+	}
 
 
-
-
-
-
-
-
-
-
-
-
-<!-- 2. AJax 통신 -->
+</script>
 
 
 </body>
 </html>
+
+<!-- 
+FileReader 란?
+FileReader는 type이 file인 input 태그 또는 API 요청과 같은 인터페이스를 통해 
+File 또는 Blob 객체를 편리하게 처리할수있는 방법을 제공하는 객체이며
+abort, load, error와 같은 이벤트에서 발생한 프로세스를 처리하는데 주로 사용되며,
+File 또는 Blob 객체를 읽어서 result 속성에 저장한다.
+
+FileReader도 비동기로 동작한다.
+
+FileReader.onload()
+load 이벤트의 핸들러. 이 이벤트는 읽기 동작이 성공적으로 완료되었을 때마다 발생한다.
+ -->
